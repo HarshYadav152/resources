019245a3db6fc2857a779cda9e8f9264125f8e82
Harsh Yadav <161936085+HarshYadav152@users.noreply.github.com>
2025-09-07 12:49:44 +0530

    Add hello.c with creative syntax for Hello World

---
+++ fun/with code/New Hello World/hello.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+#define 🧠 int
+#define 🚀 main
+#define 😀 (
+#define 😎 )
+#define 🎩 {
+#define 🪄 }
+#define 🖨 printf
+#define 📢 "Hello, World!\n"
+#define 🏁 return
+#define 🪦 0
+#define 🍕 ;
+
+🧠 🚀 😀 😎
+🎩
+    🖨 📢 🍕
+    🏁 🪦 🍕
+🪄
